5db34ebd82582d1dd1cfd68da7901ac4dd023daf
Cikgu Faiz <87230895+faizbek3991@users.noreply.github.com>
2026-05-09 06:40:25 +0000

    REST Client API

--- backend/controllers/diaryController.js
+++ backend/controllers/diaryController.js
@@ -1,21 +1,31 @@
 const Diary = require('../models/Diary');
 
 
-// CREATE
+// UPDATE CREATE
 const createDiary = async (req, res) => {
+
     try {
 
-        const diary = await Diary.create(req.body);
+        const diary = await Diary.create({
+
+            user: req.user._id,
+
+            title: req.body.title,
+
+            content: req.body.content,
+
+            mood: req.body.mood
+        });
 
         res.status(201).json(diary);
 
     } catch (error) {
+
         res.status(500).json({
             message: error.message
         });
     }
 };
-
 
 // READ ALL
 const getDiaries = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/diaries', require('./routes/diaryRoutes'));
 
 const PORT = process.env.PORT;
